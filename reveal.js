a181f8c981b02a8ea7082e46328304675d29d220
ClaraTesser-HUB <claratesserpereira@gmail.com>
2024-12-13 13:45:49 -0300

    Finalizado

---
+++ reveal.js
@@ -0,0 +1,45 @@
+window.revelar=ScrollReveal({reset:true})
+
+// frase
+revelar.reveal('.efeito-frase',{
+    duration: 2500,
+    distance: '90px',
+    delay: 500
+})
+//objetivos
+revelar.reveal('.efeito-objetivos',{
+    duration: 5000,
+    distance: '90px',
+    delay: 500,
+    origin: 'left'
+})
+
+revelar.reveal('.efeito-estrutura',{
+    duration: 2000,
+    distance: '90px',
+    origin: 'bottom'
+})
+
+revelar.reveal('.efeito-professores',{
+    duration: 2000,
+    distance: '90px',
+    origin: 'bottom',
+    delay: 200
+})
+//turma 1
+revelar.reveal('.efeito-turmas1',{
+    duration: 2000,
+    distance: '90px',
+    origin: 'bottom',
+    delay: 500
+})
+
+revelar.reveal('.efeito-turmas2',{
+    duration: 2000,
+    distance: '90px',
+    origin: 'bottom',
+})
+
+
+
+
